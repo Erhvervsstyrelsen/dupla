e02c681ba99a8796198e498e830f2d418223e006
Bjørn Göttler <bjoern@goettler.dk>
2026-08-28 15:29:15 +0200

    Reformat a bit

--- dupla/__init__.py
+++ dupla/__init__.py
@@ -1,6 +1,8 @@
 from .version import *
 from .base import *
+from .async_base import *
 from .endpoint import *
+from .async_endpoint import *
 from .exceptions import *
 from .api_keys import *
 
@@ -8,4 +10,11 @@ from . import payload
 
 extra = ["payload"]
 
-__all__ = version.__all__ + endpoint.__all__ + exceptions.__all__ + api_keys.__all__ + extra
+__all__ = (
+    version.__all__
+    + endpoint.__all__
+    + async_endpoint.__all__
+    + exceptions.__all__
+    + api_keys.__all__
+    + extra
+)
